e2bc888bc8459947b1f3d934a3332f2905698759
timon <k.poloczek7@gmail.com>
2024-11-24 22:50:07 +0100

    fix mobile navigation

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
 const nav = document.querySelector("#primary-navigation");
 const navToggle = document.querySelector(".mobile-nav-toggle");
+const body = document.querySelector("body");
 
 navToggle.addEventListener("click", () => {
    const visibility = nav.getAttribute("data-visible")
    if (visibility === "false") {
    nav.setAttribute("data-visible", true)
    navToggle.setAttribute("aria-expanded", true)
+   body.classList.toggle("no-touch")
 
  } else {
    nav.setAttribute("data-visible", false)
    navToggle.setAttribute("aria-expanded", false)
+   body.classList.toggle("no-touch")
  }
 })
